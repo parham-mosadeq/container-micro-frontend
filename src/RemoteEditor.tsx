e763077199b48152d98c77305f926335de6b4704
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2024-12-03 16:03:21 +0330

    created consumer

--- src/RemoteEditor.tsx
+++ src/RemoteEditor.tsx
@@ -1,7 +1,26 @@
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useEffect } from "react";
+import { Observable } from "windowed-observable";
 const Editor = lazy(() => import("subContainer/Editor"));
 
 export default function RemoteEditor() {
+  const observable = new Observable("auth");
+
+  const handleObserv = (msg: Record<string, any>) => {
+    console.log("host app", msg);
+    const { event, loginUser } = msg;
+    if (event === "getToken") {
+      loginUser("gotToken");
+    } else {
+      loginUser("notAllowed");
+    }
+  };
+
+  useEffect(() => {
+    observable.subscribe(handleObserv);
+
+    return () => observable.unsubscribe(handleObserv);
+  }, []);
+
   return (
     <div>
       <h2>remote:</h2>
